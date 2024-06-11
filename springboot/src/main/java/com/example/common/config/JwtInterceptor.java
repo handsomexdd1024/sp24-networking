3dbcdec38083d4447460ff4c12a0e9d611b5c0a2
Lev010608 <yanfool8321926@bupt.edu.cn>
2024-06-11 19:06:47 +0800

    恢复登录验证

--- springboot/src/main/java/com/example/common/config/JwtInterceptor.java
+++ springboot/src/main/java/com/example/common/config/JwtInterceptor.java
@@ -34,45 +34,45 @@ public class JwtInterceptor implements HandlerInterceptor {
     @Resource
     private UserService userService;
 
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-//        // 1. 从http请求的header中获取token
-//        String token = request.getHeader(Constants.TOKEN);
-//        if (ObjectUtil.isEmpty(token)) {
-//            // 如果没拿到，从参数里再拿一次
-//            token = request.getParameter(Constants.TOKEN);
-//        }
-//        // 2. 开始执行认证
-//        if (ObjectUtil.isEmpty(token)) {
-//            throw new CustomException(ResultCodeEnum.TOKEN_INVALID_ERROR);
-//        }
-//        //所有用户基类，保存账号、密码、头像
-//        Account account = null;
-//        try {
-//            // 解析token获取存储的数据
-//            String userRole = JWT.decode(token).getAudience().get(0);
-//            String userId = userRole.split("-")[0];
-//            String role = userRole.split("-")[1];
-//            // 根据userId查询数据库
-//            if (RoleEnum.ADMIN.name().equals(role)) {
-//                account = adminService.selectById(Integer.valueOf(userId));
-//            }
-//            if (RoleEnum.USER.name().equals(role)) {
-//                account = userService.selectById(Integer.valueOf(userId));
-//            }
-//        } catch (Exception e) {
-//            throw new CustomException(ResultCodeEnum.TOKEN_CHECK_ERROR);
-//        }
-//        if (ObjectUtil.isNull(account)) {
-//            throw new CustomException(ResultCodeEnum.USER_NOT_EXIST_ERROR);
-//        }
-//        try {
-//            // 用户密码加签验证 token
-//            JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(account.getPassword())).build();
-//            jwtVerifier.verify(token); // 验证token
-//        } catch (JWTVerificationException e) {
-//            throw new CustomException(ResultCodeEnum.TOKEN_CHECK_ERROR);
-//        }
-//        return true;
-//    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // 1. 从http请求的header中获取token
+        String token = request.getHeader(Constants.TOKEN);
+        if (ObjectUtil.isEmpty(token)) {
+            // 如果没拿到，从参数里再拿一次
+            token = request.getParameter(Constants.TOKEN);
+        }
+        // 2. 开始执行认证
+        if (ObjectUtil.isEmpty(token)) {
+            throw new CustomException(ResultCodeEnum.TOKEN_INVALID_ERROR);
+        }
+        //所有用户基类，保存账号、密码、头像
+        Account account = null;
+        try {
+            // 解析token获取存储的数据
+            String userRole = JWT.decode(token).getAudience().get(0);
+            String userId = userRole.split("-")[0];
+            String role = userRole.split("-")[1];
+            // 根据userId查询数据库
+            if (RoleEnum.ADMIN.name().equals(role)) {
+                account = adminService.selectById(Integer.valueOf(userId));
+            }
+            if (RoleEnum.USER.name().equals(role)) {
+                account = userService.selectById(Integer.valueOf(userId));
+            }
+        } catch (Exception e) {
+            throw new CustomException(ResultCodeEnum.TOKEN_CHECK_ERROR);
+        }
+        if (ObjectUtil.isNull(account)) {
+            throw new CustomException(ResultCodeEnum.USER_NOT_EXIST_ERROR);
+        }
+        try {
+            // 用户密码加签验证 token
+            JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(account.getPassword())).build();
+            jwtVerifier.verify(token); // 验证token
+        } catch (JWTVerificationException e) {
+            throw new CustomException(ResultCodeEnum.TOKEN_CHECK_ERROR);
+        }
+        return true;
+    }
 }
